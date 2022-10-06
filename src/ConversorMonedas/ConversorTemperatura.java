a0a78aae2a7d011b59502f02cfa45f9414242161
claudio <claudiotorancio@gmail.com>
2022-10-06 18:17:22 -0300

    Ajustando formulas de temperatura

--- src/ConversorMonedas/ConversorTemperatura.java
+++ src/ConversorMonedas/ConversorTemperatura.java
@@ -4,42 +4,42 @@ import javax.swing.JOptionPane;
 public class ConversorTemperatura {
 	
 		public void ConvertirGradosAFarenheit (double valor) {
-			double medidaF = valor / 33.8;
+			double medidaF = valor * 1.8 + 32;
 			medidaF = (double) Math.round(medidaF * 100d) /100;
 			JOptionPane.showMessageDialog(null, "Tienes " + medidaF + "°F");
 			
 		}
 		
 		public void ConvertirGradosAKelvin (double valor) {
-			double medidaK = valor / 274.15;
+			double medidaK = valor + 273.15;
 			medidaK = (double) Math.round(medidaK * 100d) /100;
 			JOptionPane.showMessageDialog(null, "Tienes " + medidaK + "K");
 			
 		}
 		
 		public void ConvertirFarenheitAGrados (double valor) {
-			double medidaC = valor / -17.22;
+			double medidaC = (valor -32) / 1.8;
 			medidaC = (double) Math.round(medidaC * 100d) /100;
 			JOptionPane.showMessageDialog(null, "Tienes " + medidaC + "°C");
 			
 		}
 		
 		public void ConvertirFarenheitAKelvin (double valor) {
-			double medidaK = valor / 255.928;
+			double medidaK =  (valor - 32) * 5/9   + 273.15;
 			medidaK = (double) Math.round(medidaK * 100d) /100;
 			JOptionPane.showMessageDialog(null, "Tienes " + medidaK + "°K");
 			
 		}
 		
 		public void ConvertirKelvinAFarenheit (double valor) {
-			double medidaF = valor / -457.87;
+			double medidaF = 1.8 *( valor -273.15) + 32;
 			medidaF = (double) Math.round(medidaF * 100d) /100;
 			JOptionPane.showMessageDialog(null, "Tienes " + medidaF + "°F");
 			
 		}
 		
 		public void ConvertirKelvinAGrados (double valor) {
-			double medidaC = valor / -272.15;
+			double medidaC = valor - 273.15;
 			medidaC = (double) Math.round(medidaC * 100d) /100;
 			JOptionPane.showMessageDialog(null, "Tienes " + medidaC + "°C");
 			
